930501706d0f90e889e942ed63980d38c84d2db7
AggieCelina <agnieszka.c.markiewicz@gmail.com>
2017-07-22 13:18:42 +0200

    fix display method

--- HabbitTracker/app/src/main/java/com/ac/habbittracker/TrackerActivity.java
+++ HabbitTracker/app/src/main/java/com/ac/habbittracker/TrackerActivity.java
@@ -3,8 +3,8 @@ package com.ac.habbittracker;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ac.habbittracker.data.TrackerContract;
 import com.ac.habbittracker.data.TrackerDbHelper;
@@ -20,13 +20,14 @@ public class TrackerActivity extends AppCompatActivity {
     }
 
     private void insertIntoRuns(){
+
         SQLiteDatabase db = trackerDbHelper.getWritableDatabase();
 
         ContentValues values = new ContentValues();
         values.put(TrackerContract.RunEntry.COLUMN_DISTANCE, "10");
         values.put(TrackerContract.RunEntry.COLUMN_UNIT, TrackerContract.RunEntry.UNIT_KM);
         values.put(TrackerContract.RunEntry.COLUMN_PACE, "7.4");
-        long newRowId = db.insert(TrackerContract.RunEntry.TABLE_NAME, null, values);
+        db.insert(TrackerContract.RunEntry.TABLE_NAME, null, values);
     }
 
     private void insertIntoFitness(){
@@ -35,22 +36,20 @@ public class TrackerActivity extends AppCompatActivity {
         ContentValues values = new ContentValues();
         values.put(TrackerContract.FitnessEntry.LENGTH, "30");
         values.put(TrackerContract.FitnessEntry.TRAININIG_TYPE, TrackerContract.FitnessEntry.TRAINING_CLIMBING);
-        long newRowId = db.insert(TrackerContract.FitnessEntry.TABLE_NAME, null, values);
+        db.insert(TrackerContract.FitnessEntry.TABLE_NAME, null, values);
     }
 
-    private void displayDatabaseInfo() {
+    public Cursor queryRunningHabits() {
+        // Create and/or open a database to read from it
         SQLiteDatabase db = trackerDbHelper.getReadableDatabase();
+
         String[] projection_runs = {
                 TrackerContract.RunEntry._ID,
                 TrackerContract.RunEntry.COLUMN_DISTANCE,
                 TrackerContract.RunEntry.COLUMN_UNIT,
                 TrackerContract.RunEntry.COLUMN_PACE};
 
-        String[] projection_fitness = {
-                TrackerContract.FitnessEntry._ID,
-                TrackerContract.FitnessEntry.LENGTH,
-                TrackerContract.FitnessEntry.TRAININIG_TYPE};
-
+        // Perform a query to return a cursor with the desired data.
         Cursor cursor_runs = db.query(
                 TrackerContract.RunEntry.TABLE_NAME,
                 projection_runs,
@@ -60,6 +59,18 @@ public class TrackerActivity extends AppCompatActivity {
                 null,
                 null);
 
+        return cursor_runs;
+    }
+
+    public Cursor queryFitnessHabits(){
+
+        SQLiteDatabase db = trackerDbHelper.getReadableDatabase();
+
+        String[] projection_fitness = {
+                TrackerContract.FitnessEntry._ID,
+                TrackerContract.FitnessEntry.LENGTH,
+                TrackerContract.FitnessEntry.TRAININIG_TYPE};
+
         Cursor cursor_fitness = db.query(
                 TrackerContract.FitnessEntry.TABLE_NAME,
                 projection_fitness,
@@ -68,6 +79,15 @@ public class TrackerActivity extends AppCompatActivity {
                 null,
                 null,
                 null);
+
+        return cursor_fitness;
+    }
+
+
+    private void displayDatabaseInfo() {
+
+        Cursor cursor_runs = queryRunningHabits();
+        Cursor cursor_fitness = queryFitnessHabits();
 
         try {
             int idColumnIndex = cursor_runs.getColumnIndex(TrackerContract.RunEntry._ID);
@@ -98,7 +118,6 @@ public class TrackerActivity extends AppCompatActivity {
             int lengthColumnIndex = cursor_fitness.getColumnIndex(TrackerContract.FitnessEntry.LENGTH);
 
             StringBuffer fitToDisplay = new StringBuffer();
-
 
             while (cursor_runs.moveToNext()) {
 
